8e690e87528d27eee818ffd6fea1fc54fae5e83b
Matt Tokunaga <matttokunaga1@gmail.com>
2025-12-05 15:59:23 -0800

    moved graphics script to scripts folder in src

---
+++ src/scripts/graphics.py
@@ -0,0 +1,14 @@
+# Add project root (one level up) to Python path
+from pathlib import Path
+import sys
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]  # go up 2 levels to repo root
+SRC_DIR = PROJECT_ROOT / "src"
+sys.path.insert(0, str(SRC_DIR))
+from dsc180a_q1_project.src.scripts import utils
+
+if __name__ == "__main__":
+    # Only averaging accuracy across two poses to save computation time, graphics in the paper were done with 100
+    utils.acc_full_test(num_poses = 2)
+    utils.acc_dist_test(num_poses = 2)
+    
